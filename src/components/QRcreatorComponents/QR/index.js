b5cf9eb332bc0ed2c8229d66a034ee921008666f
Inuk Ryjewsky <ryjewski@gmail.com>
2020-11-24 22:45:45 +0100

    addiing QR builder form

---
+++ src/components/QRcreatorComponents/QR/index.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+import QRform from "../QRform";
+var QRCode = require('qrcode.react');
+
+const QR = () => {
+  const [dataQR, setDataQR] = useState({});
+  const getValues = data => setDataQR(data)
+
+  return (
+    <>
+      <QRform getValues={getValues}/>
+      <QRCode
+          value={dataQR.valueQR || 'test'}
+          size={dataQR.sizeQR}
+          level={dataQR.hashQR}
+          fgColor={dataQR.fgQR}
+          bgColor={dataQR.bgQR}
+      />
+    </>
+  );
+};
+
+export default QR;
+
+// https://blog.qrstuff.com/2011/12/14/qr-code-error-correction#:~:text=The%20lower%20the%20error%20correction,ruggedness%20for%20general%20marketing%20use.
+
--- src/pages/QRcreator/index.js
+++ src/pages/QRcreator/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import QR from "../../Components/QRcreatorComponents/QR";
 
 const QRcreator = () => {
   return (
-      <div>
-        
-      </div>
-  );
+      <QR/>
+  )
 };
 
 export default QRcreator;
