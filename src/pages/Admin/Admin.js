f7d0df6de16d15a20c5ce26991dc2de012188838
torbjorn dahl <torbjorn@Salt-Admins-MacBook-Pro.local>
2020-11-27 12:21:02 +0100

    Some improvements for /rooms and /admin

--- src/pages/Admin/Admin.js
+++ src/pages/Admin/Admin.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from 'react';
 import Modal from 'react-modal';
 import { appContext } from '../../App';
+import NewRoomModal from './NewRoomModal/NewRoomModal';
 import './Admin.css';
 import { createMatchingRooms, submitHandler, filterCheck } from './helpers';
 import AdminSelect from './AdminSelect/AdminSelect';
@@ -9,78 +10,33 @@ Modal.setAppElement('#root');
 const Admin = () => {
   const { rooms, setRooms, filter, setFilter, users } = useContext(appContext);
   const [showModal, setShowModal] = useState(false);
-  const [newRoomId, setNewRoomId] = useState('');
-  const [newRoomName, setNewRoomName] = useState('');
-  const [newRoomFloor, setNewRoomFloor] = useState('');
   const [matchingRooms, setMatchingRooms] = useState(
     createMatchingRooms(rooms)
   );
-  console.log(newRoomId);
-
-  const newRoomSubmitHandler = (e) => {
-    e.preventDefault();
-    const newRoom = [
-      {
-        roomId: newRoomId,
-        name: newRoomName,
-        floor: newRoomFloor,
-      },
-    ];
-    console.log('Send POST to /rooms with body:');
-    console.log(JSON.stringify(newRoom));
-  };
   return (
     <div className="admin">
+      <NewRoomModal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        setFilter={setFilter}
+        rooms={rooms}
+        setRooms={setRooms}
+      ></NewRoomModal>
       <h1>ADMIN</h1>
-      <div className="primary_btn_white" onClick={() => setShowModal(true)}>
+      <button className="primary_btn_white" onClick={() => setShowModal(true)}>
         Add Room
-      </div>
-      <Modal isOpen={showModal} onRequestClose={() => setShowModal(false)}>
-        <div className="heading_medium">Add new room</div>
-        <form onSubmit={(e) => newRoomSubmitHandler(e)}>
-          <label>
-            Room Id:
-            <input
-              onChange={(e) => setNewRoomId(e.target.value)}
-              type="text"
-              name="roomId"
-            />
-          </label>
-          <label>
-            Room Name:
-            <input
-              onChange={(e) => setNewRoomName(e.target.value)}
-              type="text"
-              name="name"
-            />
-          </label>
-          <label>
-            Floor:
-            <input
-              onChange={(e) => setNewRoomFloor(e.target.value)}
-              type="text"
-              name="floor"
-            />
-          </label>
-          <button className="primary_btn_white" type="submit" value="Submit">
-            Submit
-          </button>
-        </form>
-        <button
-          className="primary_btn_white"
-          onClick={() => setShowModal(false)}
-        >
-          Close
-        </button>
-      </Modal>
+      </button>
       <div className="option_box">
-        <form onSubmit={(e) => submitHandler(e, matchingRooms, users)}>
+        <form
+          onSubmit={(e) => submitHandler(e, matchingRooms, users, setFilter)}
+        >
           <label className="search_rooms">
             <p className="paragraph">Search:</p>
             <input
               className="input_field"
               type="text"
               onChange={(e) => setFilter(e.target.value)}
+              value={filter}
             />
             <button>Use QR-Code</button>
           </label>
