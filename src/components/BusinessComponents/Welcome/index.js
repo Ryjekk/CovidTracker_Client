83d059588ed84e5e5026bc031085235c1a3128d6
Inuk Ryjewsky <ryjewski@gmail.com>
2020-11-29 14:28:29 +0100

    PartI done

--- src/components/BusinessComponents/Welcome/index.js
+++ src/components/BusinessComponents/Welcome/index.js
@@ -4,9 +4,11 @@ import '../style.css'
 const Welcome = () => {
   return (
       <div className="welcome_business">
-        <h1 className="heading_large">Time to fight COVID-19</h1>
-        <p>Use our special application to track COVID-19 risk in your company</p>
-        <div className="primary_btn_black">Start caring now</div>
+        <div className='welcome_business_wrapper'>
+          <h1 className="heading_large">Time to fight COVID-19</h1>
+          <p>Use our special application to track COVID-19 risk in your company</p>
+          <div className="primary_btn_black">Start caring now</div>
+        </div>
       </div>
   );
 };
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,29 +6,40 @@ const Footer = () => {
   return (
     <div className="wrapper_footer">
       <div className="box_footer">
-        <h3 className='heading_medium'>We are at</h3>
-        <p className='paragraph'>Lokastigur 10</p>
-        <p className='paragraph'>101 Oslo</p>
-        <p className='paragraph'>Norway</p>
+        <h3 className='heading_medium head_describer'>We are at</h3>
+        <p>Lokastigur 10</p>
+        <p>101 Oslo</p>
+        <p>Norway</p>
       </div>
       <div className="box_footer">
-        <h3 className='heading_medium'>Contact</h3>
-        <p className='paragraph'>+123 123 1234</p>
-        <p className='paragraph'>+789 789 7890</p>
-        <p className='paragraph'>xxx@xxx.com</p>
+        <h3 className='heading_medium head_describer'>Contact</h3>
+        <p>+123 123 1234</p>
+        <p>+789 789 7890</p>
+        <p>xxx@xxx.com</p>
       </div>
       <div className="box_footer">
-        <h3 className='heading_medium'>More about us</h3>
-        <p className='paragraph'>ICON</p>
-        <p className='paragraph'>ICON</p>
-        <p className='paragraph'>ICON</p>
+        <h3 className='heading_medium head_describer'>More about us</h3>
+        <div className='social'>
+          <p className='paragraph social_icons'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill='#e9e8e8'><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2 10h-2v2h2v6h3v-6h1.82l.18-2h-2v-.833c0-.478.096-.667.558-.667h1.442v-2.5h-2.404c-1.798 0-2.596.792-2.596 2.308v1.692z"/></svg>
+          </p>
+          <p className='paragraph social_icons'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill='#e9e8e8'><path d="M12 2.02c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm0 12.55l-5.992-4.57h11.983l-5.991 4.57zm0 1.288l-6-4.629v6.771h12v-6.771l-6 4.629z"/></svg>
+          </p>
+          <p className='paragraph social_icons'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill='#e9e8e8'><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2 8c0 .557-.447 1.008-1 1.008s-1-.45-1-1.008c0-.557.447-1.008 1-1.008s1 .452 1 1.008zm0 2h-2v6h2v-6zm3 0h-2v6h2v-2.861c0-1.722 2.002-1.881 2.002 0v2.861h1.998v-3.359c0-3.284-3.128-3.164-4-1.548v-1.093z"/></svg>
+          </p>
+          <p className='paragraph social_icons'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill='#e9e8e8'><path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z"/></svg>
+          </p>
+        </div>
       </div>
       <div className="box_footer">
-        <h3 className='heading_medium'>We care about your data</h3>
+        <h3 className='heading_medium head_describer'>We care about your data</h3>
         <Link to='/privacy'>
-          <p className='paragraph'>Privacy & Cookie Policy</p>
+          <p>Privacy & Cookie Policy</p>
         </Link>
-        <p className='paragraph'>Terms of service</p>
+        <p>Terms of service</p>
       </div>
     </div>
   );
