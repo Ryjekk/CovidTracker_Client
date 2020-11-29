6f260d329d5b2df13073ef9158f7b7c6b92e15c7
Inuk Ryjewsky <ryjewski@gmail.com>
2020-11-29 15:42:49 +0100

    Login /register/ retrive cstyles'

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -30,79 +30,72 @@ const Register = () => {
 
   return (
     <div className='register'>
-      <h1> Register </h1>
-      <p>Sign up for our app here</p>
+      <div>
+        <h1 className='heading_login'> Register </h1>
+        <p className='paragraph_login'>Sign up for our app here</p>
+      </div>
       <form onSubmit={logEverything}>
-        <label htmlFor='first_name'>Enter first name here:</label>
-        <br />
-        <input
-          id='first_name'
-          type='text'
-          required
-          onChange={e => {
-            firstName = e.target.value;
-          }}
-        />
-        <br />
-        <br />
-        <label htmlFor='last_name'>Enter last name here:</label>
-        <br />
-        <input
-          id='last_name'
-          type='text'
-          required
-          onChange={e => (lastName = e.target.value)}
-        />
-        <br />
-        <br />
-        <label htmlFor='email'>Enter email here:</label>
-        <br />
-        <input
-          type='email'
-          id='email'
-          required
-          onChange={e => {
-            email = e.target.value;
-          }}
-        />
-        <br />
-        <br />
-        <label htmlFor='password'>Enter password here:</label>
-        <br />
-        <input
-          id='password'
-          type='password'
-          required
-          onChange={e => {
-            password = e.target.value;
-          }}
-        />
-        <br />
-        <br />
-        <label htmlFor='repeat_password'>Repeat password here:</label>
-        <br />
-        <input
-          id='repeat_password'
-          type='password'
-          required
-          onChange={e => {
-            repeatPassword = e.target.value;
-          }}
-        />
-        <br />
-        <br />
-        <label htmlFor='companyId'>Enter your company id here:</label>
-        <br />
-        <input
-          id='companyId'
-          type='text'
-          required
-          onChange={e => {
-            companyId = e.target.value;
-          }}
-        />
-        <br />
-        <button type='submit'>Submit</button>
+        <div className='box_input'>
+          <input
+              id='first_name'
+              type='text'
+              placeholder='First name *'
+              className='inputs_main'
+              required
+              onChange={e => {
+                firstName = e.target.value;
+              }}
+          />
+          <input
+              id='last_name'
+              type='text'
+              placeholder='Last name *'
+              className='inputs_main'
+              required
+              onChange={e => (lastName = e.target.value)}
+          />
+          <input
+              type='email'
+              id='email'
+              placeholder='Email *'
+              className='inputs_main'
+              required
+              onChange={e => {
+                email = e.target.value;
+              }}
+          />
+          <input
+              id='password'
+              type='password'
+              placeholder='Password *'
+              className='inputs_main'
+              required
+              onChange={e => {
+                password = e.target.value;
+              }}
+          />
+          <input
+              id='repeat_password'
+              type='password'
+              placeholder='Repeat Password *'
+              className='inputs_main'
+              required
+              onChange={e => {
+                repeatPassword = e.target.value;
+              }}
+          />
+          <input
+              id='companyId'
+              type='text'
+              placeholder='Comapny Id *'
+              className='inputs_main'
+              required
+              onChange={e => {
+                companyId = e.target.value;
+              }}
+          />
+        </div>
+        <button type='submit' className='primary_btn_black'>Submit</button>
       </form>
     </div>
   );
