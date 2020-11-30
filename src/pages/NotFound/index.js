ca6734f7b8e71b5daefe34b71b811f8643f22cec
Inuk Ryjewsky <ryjewski@gmail.com>
2020-11-30 19:30:43 +0100

    404 page sketch design

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './style.css'
+import {Link} from "react-router-dom";
 
 const NotFound = () => {
   return (
-      <div>
-        TODO not found
+      <div className="wrapper_not">
+        <span className='for_for'>404</span>
+        <br/>
+        Looks Like You Are Lost ?
+        <div className="primary_btn_black">
+          <Link to='/'>Home </Link>
+        </div>
       </div>
   );
 };
