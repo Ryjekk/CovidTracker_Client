c55c2e5c811684ae94f63c7f35eae2466d44172b
Inuk Ryjewsky <ryjewski@gmail.com>
2020-12-01 15:46:44 +0100

    QR reader style

--- src/components/QRcomponents/QR_reader/index.js
+++ src/components/QRcomponents/QR_reader/index.js
@@ -18,14 +18,18 @@ const QR_reader = () => {
   }
 
   return (
-    <div>
+    <div className='wrapper_qr'>
+      <div className='text_qr'>
+        <h3 className='heading_medium'>QR reader</h3>
+        <p>This is a Qr reader, it will allows you to scan QR code inside your company</p>
+      </div>
       <QrReader
           delay={300}
           onError={handleError}
           onScan={handleScan}
           style={{ width: '100%' }}
       />
-      <p>{scanResult || scanError}</p>
+      <p className="heading_medium">{scanResult || scanError}</p>
     </div>
   );
 };
