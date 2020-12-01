43f38340cf1a963732183f0e7d85e47ef0b446e4
torbjorn dahl <torbjorn@Salt-Admins-MacBook-Pro.local>
2020-12-01 16:23:02 +0100

    QR-Reader now sending request and redirecting to daashboard

--- src/components/QRcomponents/QR_reader/index.js
+++ src/components/QRcomponents/QR_reader/index.js
@@ -1,29 +1,45 @@
-import React, {useState} from 'react';
-import QrReader from 'react-qr-reader'
-
+import React, { useState, useContext } from 'react';
+import QrReader from 'react-qr-reader';
+import { appContext } from '../../../App.js';
+import { addRoomToUser } from './remote';
+import { Redirect } from 'react-router-dom';
 const QR_reader = () => {
-  const [scanResult, setScanResult] = useState('')
-  const [scanError, setScanError] = useState('')
+  const [scanResult, setScanResult] = useState('');
+  const [scanError, setScanError] = useState('');
+  const [redirect, setRedirect] = useState(false);
 
-  const handleScan = data => {
-    // TODO delete
-    console.log(data)
+  const { users } = useContext(appContext);
+
+  const handleScan = async (data) => {
     if (data) {
-      setScanResult(data)
+      const date = new Date();
+      const dateString = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
+      const time = `${date.getHours()}:${date.getMinutes()}`;
+      const responseBody = {
+        _id: users._id,
+        rooms: [{ _id: data, date: dateString, time: time }],
+      };
+      const responseCode = await addRoomToUser(responseBody, users);
+      if (responseCode === 200) {
+        console.log('Success');
+        setRedirect(true);
+      }
     }
-  }
-  const handleError = err => {
-    console.error(err)
-    setScanError(err.message)
-  }
+  };
+
+  const handleError = (err) => {
+    console.error(err);
+    setScanError(err.message);
+  };
 
   return (
     <div>
+      {redirect ? <Redirect to="/dashboard"></Redirect> : null}
       <QrReader
-          delay={300}
-          onError={handleError}
-          onScan={handleScan}
-          style={{ width: '100%' }}
+        delay={300}
+        onError={handleError}
+        onScan={handleScan}
+        style={{ width: '100%' }}
       />
       <p>{scanResult || scanError}</p>
     </div>
