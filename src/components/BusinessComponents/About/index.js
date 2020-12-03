663f8807b98a620e71d58eae08739a8dbd51bf0a
torbjorn dahl <torbjorn@Salt-Admins-MacBook-Pro.local>
2020-12-03 12:34:14 +0100

    Fixing typos, replacing dummytext, fixed print-page, added favicon

--- src/components/BusinessComponents/About/index.js
+++ src/components/BusinessComponents/About/index.js
@@ -1,36 +1,44 @@
 import React from 'react';
-import img1 from "../../../Assets/background/businessImg1.jpg";
-import img2 from "../../../Assets/background/businessImg2.jpg";
-import '../style.css'
-import Stats from "../Stats";
+import img1 from '../../../Assets/background/businessImg1.jpg';
+import img2 from '../../../Assets/background/businessImg2.jpg';
+import '../style.css';
+import Stats from '../Stats';
 
 const About = () => {
   return (
-      <div className="content_business">
-        <Stats/>
-        <hr className="separator"/>
-        <div className="about_business">
-          <img className="img_business" src={img1} alt="img COVID"/>
-          <div className="textBox_business_right">
-            <h3 className="heading_medium">Help your employees to stay safe</h3>
-            <p className="paragraph">
-              Find the right candidate quickly. We will match you only with the candidates that perfectly fulfil your needs, which will cut down the hiring time. We know our students and graduates very well, and our advanced algorithm will connect you with the best candidate for your business.
-            </p>
-            <div className="primary_btn_black">Start caring now</div>
-          </div>
-        </div>
-
-        <div className="about_business">
-          <div className="textBox_business_left">
-            <h3 className="heading_medium">Help your employees to stay safe</h3>
-            <p className="paragraph">
-              Find the right candidate quickly. We will match you only with the candidates that perfectly fulfil your needs, which will cut down the hiring time. We know our students and graduates very well, and our advanced algorithm will connect you with the best candidate for your business.
-            </p>
-            <div className="primary_btn_black">Start caring now</div>
-          </div>
-          <img className="img_business" src={img2} alt="imageCOVID"/>
+    <div className="content_business">
+      <Stats />
+      <hr className="separator" />
+      <div className="about_business">
+        <img className="img_business" src={img1} alt="img COVID" />
+        <div className="textBox_business_right">
+          <h3 className="heading_medium">Help your employees stay safe</h3>
+          <p className="paragraph">
+            Make your employees feel comfortable coming to work. With{' '}
+            <span className="novid">Novid</span>, potential covid outbreaks can
+            be stopped the moment they begin. When a person gets a positive
+            test, all potential contacts are sent into quarantine, so that the
+            rest can safely keep coming back. Its important for employees to
+            have a safe work environment.
+          </p>
+          <div className="primary_btn_black">Start caring now</div>
         </div>
       </div>
+
+      <div className="about_business">
+        <div className="textBox_business_left">
+          <h3 className="heading_medium">Boost your business</h3>
+          <p className="paragraph">
+            Studies have shown that productivity at many companies has dropped
+            with as much as 20% after going remote. Let us take care of
+            infection tracking, so that you can focus on making your business
+            grow.
+          </p>
+          <div className="primary_btn_black">Start caring now</div>
+        </div>
+        <img className="img_business" src={img2} alt="imageCOVID" />
+      </div>
+    </div>
   );
 };
 
--- src/pages/Admin/ComponentToPrint/ComponentToPrint.js
+++ src/pages/Admin/ComponentToPrint/ComponentToPrint.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './ComponentToPrint.css';
+import logo from '../../../Assets/logo.png';
 const QRCode = require('qrcode.react');
 
 class ComponentToPrint extends React.Component {
   render() {
     return (
       <div className="qr_print">
-        <div className="heading_medium ">Some ads to make us rich</div>
-        <div className="heading_medium ">Our name and logo</div>
+        <img className="print_logo" src={logo} />
+        <div className="heading_large">Novid</div>
         <QRCode
           id={this.props._id.toString()}
           value={this.props._id.toString()}
